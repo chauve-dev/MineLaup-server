a9fdd407e86c784029ce8b1b82e1f78aaf02834a
Louis Bailleau <louis.bailleau@free.fr>
2020-04-26 00:01:53 +0200

    Initialized NuxtJs project and install some dependencies
    - Initialized the NuxtJS project
    - Created tailwindcss config file
    - Installed `nuxt-i18n` and `tailwindcss-dark-mode`
    - Closed #1

---
+++ README.md
@@ -0,0 +1,22 @@
+# minelaup-server
+
+> The server side of the project MineLaup
+
+## Build Setup
+
+```bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  theme: {
+    extend: {},
+    darkSelector: '.dark-mode'
+  },
+  variants: {
+    backgroundColor: [
+      'dark',
+      'dark-hover',
+      'dark-group-hover',
+      'dark-even',
+      'dark-odd',
+      'hover'
+    ],
+    borderColor: ['dark', 'dark-focus', 'dark-focus-within', 'hover'],
+    textColor: [
+      'dark',
+      'dark-hover',
+      'dark-active',
+      'hover',
+      'dark-placeholder'
+    ]
+  },
+  plugins: [require('tailwindcss-dark-mode')()]
+}
