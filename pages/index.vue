5b578ac914c187166f26a6874bdb960e7e7db992
Louis Bailleau <louis.bailleau@free.fr>
2020-04-26 11:13:04 +0200

    Migrate all page to Typescript and fixed linter
    - Migrate vue page to Typescript with `nuxt-properties-decorator`
    - Fixed linter and lint all files
    - Upgraded all packages

--- pages/index.vue
+++ pages/index.vue
@@ -25,14 +25,15 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { Vue, Component } from 'nuxt-property-decorator'
 import Logo from '~/components/Logo.vue'
 
-export default Vue.extend({
+@Component({
   components: {
-    Logo
-  }
+    Logo,
+  },
 })
+export default class Index extends Vue {}
 </script>
 
 <style>
