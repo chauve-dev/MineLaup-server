40d27f0e34b11edf6be998ce70d5b8078d0b7b9f
Louis Bailleau <louis.bailleau@free.fr>
2020-04-27 14:07:52 +0200

    Updated tests for jest

--- test/e2e/index.spec.js
+++ test/e2e/index.spec.js
@@ -1,5 +1,4 @@
 import { resolve } from 'path'
-import test from 'ava'
 import { Nuxt, Builder } from 'nuxt'
 
 // We keep the nuxt and server instance
@@ -7,23 +6,25 @@ import { Nuxt, Builder } from 'nuxt'
 let nuxt = null
 
 // Init Nuxt.js and create a server listening on localhost:4000
-test.before(async () => {
-  const config = {
-    dev: false,
-    rootDir: resolve(__dirname, '../../'),
-  }
-  nuxt = new Nuxt(config)
-  await new Builder(nuxt).build()
-  await nuxt.server.listen(4000, 'localhost')
-}, 30000)
+describe('Init server for e2e', () => {
+  beforeEach(async () => {
+    const config = require('../../nuxt.config')
+    config.dev = false
+    config.rootDir = resolve(__dirname, '../../')
 
-// Example of testing only generated html
-test('Route / exits and render HTML', async (t) => {
-  const { html } = await nuxt.renderRoute('/', {})
-  t.true(html.includes('Documentation'))
-})
+    nuxt = new Nuxt(config)
+    await nuxt.ready()
+    await new Builder(nuxt).build()
+    await nuxt.server.listen(4000, 'localhost')
+  }, 30000)
 
-// Close server and ask nuxt to stop listening to file changes
-test.after('Closing server and nuxt.js', (_t) => {
-  nuxt.close()
+  it('Route / exits and render HTML', async () => {
+    const { html } = await nuxt.renderRoute('/', {})
+    expect(html.includes('Documentation')).toBe(true)
+  })
+
+  // Close server and ask nuxt to stop listening to file changes
+  afterEach(() => {
+    nuxt.close()
+  })
 })
